eadfc07fc35e5d0c23af2419ce06c4db799b0539
Mamadou-Sylla <momosylla1998@gmail.com>
2023-02-17 16:02:44 +0000

    gastro-and-me-back can't running bugfix 1

---
+++ gastro-and-me/src/main/java/com/gastroandmeback/GastroAndMeBackApplication.java
@@ -0,0 +1,13 @@
+package com.gastroandmeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroAndMeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroAndMeBackApplication.class, args);
+	}
+
+}
